cab0e58bcdc2e667e695ffd903e9063f4cb9e38f
Rahul kumar <rahulkumar208>
2024-08-19 14:27:12 +0200

    Push python code

--- Task_1/battery_power.py
+++ Task_1/battery_power.py
@@ -1,33 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "d459e1db-d8dd-470c-a51f-46a5b845b369",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# Battery Power
+
+def battery_power(throttle_level):
+    
+    # Check for invalid throttle levels
+    if throttle_level < 0 or throttle_level > 250:
+        raise ValueError("Invalid throttle level: Must be between 0 and 250 km/h")
+    
+    # Calculate the power level of the battery
+    power_level = (throttle_level / 250) * 1000
+    
+    return power_level
+
+# Example Test Cases
+print(battery_power(0))      # Expected output: 0 watts (Throttle at MIN LEVEL)
+print(battery_power(125))    # Expected output: 500 watts (Throttle at mid-level)
+print(battery_power(250))    # Expected output: 1000 watts (Throttle at MAX LEVEL)
